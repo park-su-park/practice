db6ebed7e02f59946d037193413d900feaedcb9c
rlatnfla <rlatnfla0724@gmail.com>
2025-02-13 04:14:10 +0900

    feat: JpaAuditing 활성화

--- backend/src/main/java/park_su_park/backend/BackendApplication.java
+++ backend/src/main/java/park_su_park/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package park_su_park.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendApplication {
 
 	public static void main(String[] args) {
