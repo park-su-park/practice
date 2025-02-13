5a3d01348b93c4585043baed832b8de82d3dea04
rlatnfla <rlatnfla0724@gmail.com>
2025-02-13 16:19:02 +0900

    feat: 예상치 못한 예외가 발생 했을때 처리 및 로깅할 핸들러 추가

--- backend/src/main/java/park_su_park/backend/GlobalExceptionHandler.java
+++ backend/src/main/java/park_su_park/backend/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package park_su_park.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import park_su_park.backend.exception.DuplicateResourceException;
 import park_su_park.backend.exception.ResourceNotFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({DuplicateResourceException.class, IllegalArgumentException.class})
@@ -18,5 +20,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleUnknownError(Exception e) {
+        log.error("Unexpected error occurred : {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 오류가 발생했습니다.");
     }
 }
