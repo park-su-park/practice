d06bebb5d6c888e9c0cebdcc8122c9a09e32d46a
rlatnfla <rlatnfla0724@gmail.com>
2025-02-13 16:20:33 +0900

    chore: lombok 사용하여 코드 간결화

--- backend/src/main/java/park_su_park/backend/dto/UserDataResponse.java
+++ backend/src/main/java/park_su_park/backend/dto/UserDataResponse.java
@@ -1,23 +1,19 @@
 package park_su_park.backend.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import park_su_park.backend.domain.User;
 
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserDataResponse {
 
     private final String username;
     private final String email;
     private final LocalDateTime createdTime;
-
-    //==생성자==//
-    private UserDataResponse(String username, String email, LocalDateTime createdTime) {
-        this.username = username;
-        this.email = email;
-        this.createdTime = createdTime;
-    }
 
     //==생성 메서드==//
     public static UserDataResponse createUserDataResponse(User user) {
