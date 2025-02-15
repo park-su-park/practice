aa0695b84a0ea703718acddaea75551c79025d9c
rlatnfla <rlatnfla0724@gmail.com>
2025-02-15 20:13:06 +0900

    feat: add auditing fields (createdTime, updateTime) for JPA auditing feat: add static factory method, association mapping method, business logic

--- backend/src/main/java/park_su_park/backend/domain/ToDo.java
+++ backend/src/main/java/park_su_park/backend/domain/ToDo.java
@@ -2,7 +2,12 @@ package park_su_park.backend.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import park_su_park.backend.dto.requestBody.CreateToDoRequest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -10,6 +15,8 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
+@EntityListeners({AuditingEntityListener.class})
+@NoArgsConstructor
 public class ToDo {
     @Id @GeneratedValue
     @Column(name = "to_do_id")
@@ -26,7 +33,33 @@ public class ToDo {
 
     private String content;
 
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdTime;
 
+    @LastModifiedDate
     private LocalDateTime updateTime;
+
+    //==생성자==//
+    private ToDo(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    //==생성 메서드==//
+    public static ToDo create(CreateToDoRequest createToDoRequest) {
+        return new ToDo(createToDoRequest.getTitle(), createToDoRequest.getContent());
+    }
+
+    //==연관 관계 매핑 메서드==//
+    public void setUser(User user) {
+        this.user = user;
+        user.getToDos().add(this);
+    }
+
+    //==비즈니스 로직==//
+    public void update(CreateToDoRequest updateToDoRequest) {
+        this.title = updateToDoRequest.getTitle();
+        this.content = updateToDoRequest.getContent();
+    }
 }
