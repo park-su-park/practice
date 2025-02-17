bca19e288fd7910e2bc6da6b3acd8a68049a4e5d
rlatnfla <rlatnfla0724@gmail.com>
2025-02-18 03:17:03 +0900

    refactor: introduce new response dto class ApiResponseBody

--- backend/src/main/java/park_su_park/backend/GlobalExceptionHandler.java
+++ backend/src/main/java/park_su_park/backend/GlobalExceptionHandler.java
@@ -3,40 +3,39 @@ package park_su_park.backend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import park_su_park.backend.exception.DuplicateResourceException;
-import park_su_park.backend.exception.ForbiddenAccessException;
-import park_su_park.backend.exception.ResourceNotFoundException;
-import park_su_park.backend.exception.UnauthorizedAccessException;
+import park_su_park.backend.dto.responseBody.ApiResponseBody;
+import park_su_park.backend.exception.*;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({DuplicateResourceException.class, IllegalArgumentException.class})
-    public ResponseEntity<String> handleBadRequest(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    @ExceptionHandler({DuplicateResourceException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ApiResponseBody> handleBadRequest(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponseBody.failure(e.getMessage()));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleNotFound(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ApiResponseBody> handleNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponseBody.failure(e.getMessage()));
     }
 
-    @ExceptionHandler(UnauthorizedAccessException.class)
-    public ResponseEntity<String> handleUnauthorized(Exception e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    @ExceptionHandler({UnauthorizedAccessException.class, AuthenticationFailedException.class})
+    public ResponseEntity<ApiResponseBody> handleUnauthorized(Exception e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponseBody.failure(e.getMessage()));
     }
 
     @ExceptionHandler(ForbiddenAccessException.class)
-    public ResponseEntity<String> handleForbidden(Exception e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    public ResponseEntity<ApiResponseBody> handleForbidden(Exception e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiResponseBody.failure(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleUnknownError(Exception e) {
+    public ResponseEntity<ApiResponseBody> handleUnknownError(Exception e) {
         log.error("Unexpected error occurred : {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 오류가 발생했습니다.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponseBody.failure("서버 내부 오류 발생"));
     }
 }
