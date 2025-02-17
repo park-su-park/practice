351db2c79c6e05d201208c38282dcc95c80776fd
rlatnfla <rlatnfla0724@gmail.com>
2025-02-18 03:07:11 +0900

    feat: add constant message class

---
+++ backend/src/main/java/park_su_park/backend/util/message/AuthResponseMessage.java
@@ -0,0 +1,7 @@
+package park_su_park.backend.util.message;
+
+public class AuthResponseMessage {
+    public final static String SIGN_IN_SUCCESS = "회원가입 성공";
+    public final static String LOGIN_SUCCESS = "로그인 성공";
+    public final static String LOGOUT_SUCCESS = "로그아웃 성공";
+}
