824a7757233d697743728ed8046f6688aba9bcf1
rlatnfla <rlatnfla0724@gmail.com>
2025-02-18 03:07:11 +0900

    feat: add constant message class

---
+++ backend/src/main/java/park_su_park/backend/util/message/ToDoResponseMessage.java
@@ -0,0 +1,8 @@
+package park_su_park.backend.util.message;
+
+public class ToDoResponseMessage {
+    public static final String TODO_CREATION_SUCCESS = "일정 생성 성공";
+    public static final String TODO_FETCH_SUCCESS = "일정 조회 성공";
+    public static final String TODO_UPDATE_SUCCESS = "일정 수정 성공";
+    public static final String TODO_DELETION_SUCCESS = "일정 삭제 성공";
+}
