797e0acbd7c1fc727fe60a1e7a3c53c7d8cb1694
rlatnfla <rlatnfla0724@gmail.com>
2025-02-18 03:16:51 +0900

    refactor: introduce new response dto class ApiResponseBody

--- backend/src/main/java/park_su_park/backend/controller/CommentController.java
+++ backend/src/main/java/park_su_park/backend/controller/CommentController.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import park_su_park.backend.dto.requestBody.CreateCommentRequest;
-import park_su_park.backend.dto.responseBody.CommentDataResponse;
+import park_su_park.backend.dto.responseBody.ApiResponseBody;
+import park_su_park.backend.dto.responseBody.CommentData;
 import park_su_park.backend.service.CommentService;
+import park_su_park.backend.util.message.CommentResponseMessage;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,30 +20,30 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/create/{toDoId}")
-    public ResponseEntity<CommentDataResponse> createComment(@RequestBody @Valid CreateCommentRequest createCommentRequest, @PathVariable Long toDoId, HttpSession session) {
-        CommentDataResponse commentDataResponse = commentService.createComment(createCommentRequest, toDoId, session);
+    public ResponseEntity<ApiResponseBody> createComment(@RequestBody @Valid CreateCommentRequest createCommentRequest, @PathVariable Long toDoId, HttpSession session) {
+        CommentData commentData = commentService.createComment(createCommentRequest, toDoId, session);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(commentDataResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponseBody.success(CommentResponseMessage.COMMENT_CREATION_SUCCESS, commentData));
     }
 
     @GetMapping("/{commentId}")
-    public ResponseEntity<CommentDataResponse> getComment(@PathVariable Long commentId) {
-        CommentDataResponse commentDataResponse = commentService.getComment(commentId);
+    public ResponseEntity<ApiResponseBody> getComment(@PathVariable Long commentId) {
+        CommentData commentData = commentService.getComment(commentId);
 
-        return ResponseEntity.ok(commentDataResponse);
+        return ResponseEntity.ok(ApiResponseBody.success(CommentResponseMessage.COMMENT_FETCH_SUCCESS, commentData));
     }
 
     @PatchMapping("/{commentId}")
-    public ResponseEntity<CommentDataResponse> updateComment(@RequestBody @Valid CreateCommentRequest updateCommentRequest, @PathVariable Long commentId, HttpSession session) {
-        CommentDataResponse commentDataResponse = commentService.updateComment(updateCommentRequest, commentId, session);
+    public ResponseEntity<ApiResponseBody> updateComment(@RequestBody @Valid CreateCommentRequest updateCommentRequest, @PathVariable Long commentId, HttpSession session) {
+        CommentData commentData = commentService.updateComment(updateCommentRequest, commentId, session);
 
-        return ResponseEntity.ok(commentDataResponse);
+        return ResponseEntity.ok(ApiResponseBody.success(CommentResponseMessage.COMMENT_UPDATE_SUCCESS, commentData));
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<Void> deleteComment(@PathVariable Long commentId, HttpSession session) {
+    public ResponseEntity<ApiResponseBody> deleteComment(@PathVariable Long commentId, HttpSession session) {
         commentService.deleteComment(commentId, session);
 
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.ok(ApiResponseBody.success(CommentResponseMessage.COMMENT_DELETION_SUCCESS, null));
     }
 }
