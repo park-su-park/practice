735d7bee6049a2ca677602036c71b25504077e87
rlatnfla <rlatnfla0724@gmail.com>
2025-02-18 19:14:33 +0900

    refactor: delete unused endpoint

--- backend/src/main/java/park_su_park/backend/controller/UserController.java
+++ backend/src/main/java/park_su_park/backend/controller/UserController.java
@@ -1,13 +1,8 @@
 package park_su_park.backend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import park_su_park.backend.domain.User;
-import park_su_park.backend.dto.responseBody.UserDataResponse;
 import park_su_park.backend.service.UserService;
 
 @RestController
@@ -16,20 +11,4 @@ import park_su_park.backend.service.UserService;
 public class UserController {
 
     private final UserService userService;
-
-    @GetMapping("/search")
-    public ResponseEntity<UserDataResponse> searchUser(
-            @RequestParam String q,
-            @RequestParam String udm) {
-
-        if(udm.equals("username")){
-            User user = userService.findUserByUsername(q);
-            return ResponseEntity.ok(UserDataResponse.createUserDataResponse(user));
-        } else if (udm.equals("email")) {
-            User user = userService.findUserByEmail(q);
-            return ResponseEntity.ok(UserDataResponse.createUserDataResponse(user));
-        } else {
-            throw new IllegalArgumentException("유효한 검색 타입을 지정해서 검색해주세요.");
-        }
-    }
 }
