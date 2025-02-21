c432088b8938d547a29d972c0fe52e82e8555430
rlatnfla <rlatnfla0724@gmail.com>
2025-02-21 16:55:46 +0900

    refactor: change regexp's path

--- backend/src/main/java/park_su_park/backend/util/validation/Password.java
+++ backend/src/main/java/park_su_park/backend/util/validation/Password.java
@@ -5,6 +5,7 @@ import jakarta.validation.Payload;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import park_su_park.backend.util.constant.ValidationMessage;
+import park_su_park.backend.util.constant.ValidationRegExp;
 
 import java.lang.annotation.*;
 
@@ -13,7 +14,7 @@ import java.lang.annotation.*;
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Size(min = 8, max = 20, message = ValidationMessage.PASSWORD_SIZE_MESSAGE)
-@Pattern(regexp = ValidationMessage.PASSWORD_REGEXP, message = ValidationMessage.PASSWORD_PATTER_MESSAGE)
+@Pattern(regexp = ValidationRegExp.PASSWORD_REGEXP, message = ValidationMessage.PASSWORD_PATTER_MESSAGE)
 public @interface Password {
     String message() default ValidationMessage.DEFAULT_PASSWORD_VALIDATION_FAILED_MESSAGE;
     Class<?>[] groups() default {};
